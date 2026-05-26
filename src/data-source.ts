071cc02327e262c240264d8f7aa220d3e162da5b
Sergey Riedel <sergey.riedel@gmail.com>
2026-05-26 16:31:44 +0200

    Move db

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,7 +5,7 @@ import { Comment } from './comments/comments.entity';
 
 export const AppDataSource = new DataSource({
   type: 'better-sqlite3',
-  database: '../data/db.sqlite',
+  database: './data/db.sqlite',
   entities: [Thread, Comment],
   migrations: ['src/migrations/*.ts'],
   synchronize: false, // Absolutely critical to disable this here
